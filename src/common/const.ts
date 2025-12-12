4ff804bfe3131ad5f5b01321a9bc0ae7fa52d516
Duong <pduongpdu99@gmail.com>
2025-12-12 23:26:21 +0700

    feat: add const configuration

---
+++ src/common/const.ts
@@ -0,0 +1,25 @@
+import { IConfiguration } from "@i/const";
+
+const CONFIGURATION: IConfiguration = {
+    DEFAULT_VALUE: {
+        PAGINATION: {
+            LIMIT: "10",
+            PAGE: "1",
+            ORDER: "id desc"
+        },
+    },
+    DATABASE: {
+        MYSQL: {
+            DATABASE: "db_temporary",
+            HOST: "127.0.0.1",
+            PASSWORD: "Aa123456@",
+            PORT: 3306,
+            USERNAME: "root"
+        }
+    },
+    REDIS: {
+
+    }
+}
+
+export default CONFIGURATION;
---
+++ src/common/interfaces/const.ts
@@ -0,0 +1,25 @@
+export interface DB {
+    HOST: string;
+    PORT: number;
+    USERNAME: string;
+    PASSWORD: string;
+    DATABASE: string;
+}
+
+export interface IConfiguration {
+    DEFAULT_VALUE: {
+        PAGINATION: {
+            LIMIT: string;
+            PAGE: string;
+            ORDER?: string;
+            Q?: string;
+        };
+    },
+    DATABASE: {
+        MYSQL?: DB
+        MONGO?: Record<string, string>
+    },
+    REDIS: {
+
+    }
+}
