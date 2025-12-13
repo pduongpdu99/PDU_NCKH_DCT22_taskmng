2d980c5585eaf78977b6a69085b3d75a655ec897
Duong <pduongpdu99@gmail.com>
2025-12-13 12:35:38 +0700

    update CRUD

--- src/common/impl/CRUD.ts
+++ src/common/impl/CRUD.ts
@@ -1,21 +1,51 @@
-import { ICRUD } from "@i/CRUD";
-import { IQuery } from "@i/query";
+import { ParamHelper } from '@helpers/param'
+import { ICRUD } from '@i/CRUD'
+import { IQuery } from '@i/query'
+import { Repository } from 'typeorm'
 
-export abstract class CRUDImpl<C, U, E> implements ICRUD<C, U, E> {
-    find(query: IQuery): Promise<[E]> {
-        throw new Error("Method not implemented.");
-    }
-    findById(): Promise<E> {
-        throw new Error("Method not implemented.");
-    }
-    create(payload: C): Promise<E> {
-        throw new Error("Method not implemented.");
-    }
-    update(id: number, payload: U): Promise<E> {
-        throw new Error("Method not implemented.");
-    }
-    delete(id: number): null {
-        throw new Error("Method not implemented.");
-    }
+export abstract class CRUDImpl<
+  C extends E,
+  U extends E,
+  E extends Record<string, any>,
+> implements ICRUD<C, U, E> {
+  repo: Repository<E>
+  alias: string = ''
+  paramHelper: ParamHelper
 
+  async find(query: Record<string, string>) {
+    const queryFormatted: IQuery = this.paramHelper.parse(query)
+    let builder = this.repo.createQueryBuilder(this.alias)
+
+    queryFormatted.filter.forEach(([k, v]) => {
+      if (k.endsWith('_id') || typeof v === 'number' || k === 'id') {
+        builder = builder.where(`${this.alias}.${k} = :param`, {
+          param: v,
+        })
+      } else {
+        builder = builder.where(`${this.alias}.${k} LIKE :param`, {
+          param: v,
+        })
+      }
+    })
+
+    return builder.getMany()
+  }
+
+  async findById(id: number) {
+    let builder = this.repo.createQueryBuilder(this.alias)
+    builder = builder.where(`${this.alias}.id = :param`, { param: id })
+    return builder.getOne()
+  }
+
+  create(payload: C) {
+    return this.repo.insert(payload)
+  }
+
+  update(id: number, payload: U) {
+    return this.repo.update(id, payload)
+  }
+
+  async delete(id: number) {
+    await this.repo.delete(id)
+  }
 }
--- src/common/interfaces/CRUD.ts
+++ src/common/interfaces/CRUD.ts
@@ -1,7 +1,5 @@
-import { IQuery } from './query'
-
 export interface ICRUD<C, U, E> {
-  find(query: IQuery): Promise<E[]>
+  find(query: Record<string, string>): Promise<E[]>
   findById(id: number): Promise<E | null>
   create(payload: C): unknown
   update(id: number, payload: U): unknown
