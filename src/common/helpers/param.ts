f8b6ede7e57e74d77f1fec155e1e8ad64c8eed37
Duong <pduongpdu99@gmail.com>
2025-12-13 12:22:11 +0700

    update: ParamHelper parse

--- src/common/helpers/param.ts
+++ src/common/helpers/param.ts
@@ -1,15 +1,31 @@
-import { IQuery } from "@i/query";
-import { ConfigService } from '@nestjs/config';
-import CONFIGURATION from "@common/const";
+import { IQuery } from '@i/query'
+import { ConfigService } from '@nestjs/config'
+import CONFIGURATION from '@common/const'
 
 export abstract class ParamHelper {
-    constructor(private readonly config: ConfigService) { }
-    normalize(query: Record<string, string>): IQuery {
-        return {
-            limit: Number(query.limit ?? this.config.get(CONFIGURATION.DEFAULT_VALUE.PAGINATION.LIMIT)),
-            page: Number(query.limit ?? this.config.get(CONFIGURATION.DEFAULT_VALUE.PAGINATION.PAGE)),
-            order: "id desc",
-            q: ""
-        }
+  constructor(private readonly config: ConfigService) {}
+  parser(query: Record<string, string>): IQuery {
+    const { limit, page, order, all, q, ...filter } = query
+
+    const _limit = Number(
+      limit ?? this.config.get(CONFIGURATION.DEFAULT_VALUE.PAGINATION.LIMIT),
+    )
+    const _page = Number(
+      page ?? this.config.get(CONFIGURATION.DEFAULT_VALUE.PAGINATION.PAGE),
+    )
+
+    const _order =
+      order ??
+      this.config.get(CONFIGURATION.DEFAULT_VALUE.PAGINATION.ORDER || 'id desc')
+
+    return {
+      limit: _limit,
+      page: _page,
+      skip: ((_page <= 0 ? 1 : _page) - 1) * _limit,
+      order: _order,
+      q: q ?? '',
+      all: all === 'true',
+      filter: Object.entries(filter),
     }
+  }
 }
